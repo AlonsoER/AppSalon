28bb2168a29cea3fd8172abc1bcb514c576569a8
AlonsoER <alonsoer2343@gmail.com>
2022-09-11 12:50:27 -0600

    Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b8aec76e43569f7dacbcef0544bc927aea2a4ec7',
+        'name' => 'hp-gamer/app-salon',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hp-gamer/app-salon' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b8aec76e43569f7dacbcef0544bc927aea2a4ec7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
